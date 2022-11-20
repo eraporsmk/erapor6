8b4432fdcca293a7995d451334b6838ff4411f0a
masadi <masadi.com@gmail.com>
2022-11-20 13:11:26 +0700

    Update Cetak Rapor UTS

--- app/Http/Livewire/Laporan/RaporUts.php
+++ app/Http/Livewire/Laporan/RaporUts.php
@@ -11,16 +11,21 @@ use App\Models\Rapor_pts;
 class RaporUts extends Component
 {
     use LivewireAlert;
+    public $show = FALSE;
     public $collection = [];
+    public $semester_id;
+    public $tingkat;
     public $rombongan_belajar_id;
     public $rencana_penilaian = [];
-    
+    public $data_rombongan_belajar = [];
+
     protected $listeners = [
         'confirmed' => '$refresh'
     ];
 
     public function render()
     {
+        $this->semester_id = session('semester_id');
         return view('livewire.laporan.rapor-uts', [
             'breadcrumbs' => [
                 ['link' => "/", 'name' => "Beranda"], ['link' => '#', 'name' => 'Laporan'], ['name' => "Cetak Rapor UTS"]
@@ -42,15 +47,30 @@ class RaporUts extends Component
         foreach($this->collection as $item){
             $this->rencana_penilaian[$item->pembelajaran_id] = ($item->rapor_pts->count()) ? $item->rapor_pts->pluck('rencana_penilaian_id')->toArray() : '';
         }
+        if($this->loggedUser()->hasRole('waka', session('semester_id'))){
+            $this->show = FALSE;
+        } elseif($this->check_walas()){
+            $this->show = TRUE;
+        }
+        //$this->show = $this->check_walas($this->rombongan_belajar_id);
     }
     public function loggedUser(){
         return auth()->user();
     }
-    private function check_walas(){
-        if($this->loggedUser()->hasRole('wali', session('semester_id'))){
-            return TRUE;
+    private function check_walas($rombongan_belajar_id = NULL){
+        if($rombongan_belajar_id){
+            $rombongan_belajar = Rombongan_belajar::find($rombongan_belajar_id);
+            if($rombongan_belajar->guru_id == $this->loggedUser()->guru_id){
+                return TRUE;
+            } else {
+                return FALSE;
+            }
         } else {
-            return FALSE;
+            if($this->loggedUser()->hasRole('wali', session('semester_id'))){
+                return TRUE;
+            } else {
+                return FALSE;
+            }
         }
     }
     public function getRombel(){
@@ -61,8 +81,55 @@ class RaporUts extends Component
             $query->where('sekolah_id', session('sekolah_id'));
         })->first();
     }
+    public function updatedTingkat(){
+        $this->reset(['data_rombongan_belajar', 'rombongan_belajar_id', 'collection', 'rencana_penilaian']);
+        if($this->tingkat){
+            $data_rombongan_belajar = Rombongan_belajar::select('rombongan_belajar_id', 'nama')->where(function($query){
+                $query->where('tingkat', $this->tingkat);
+                $query->where('semester_id', session('semester_aktif'));
+                $query->where('sekolah_id', session('sekolah_id'));
+                $query->where('jenis_rombel', 1);
+            })->get();
+            $this->dispatchBrowserEvent('data_rombongan_belajar', ['data_rombongan_belajar' => $data_rombongan_belajar]);
+        }
+    }
+    public function updatedRombonganBelajarId(){
+        $this->reset(['collection', 'rencana_penilaian']);
+        $this->collection = Pembelajaran::with([
+            'guru', 
+            'rencana_penilaian' => function($query) {
+                $query->where('kompetensi_id', 1);
+            },
+            'rapor_pts',
+            'rombongan_belajar',
+        ])->whereNotNull('kelompok_id')->whereNotNull('no_urut')->whereHas('rombongan_belajar', function($query){
+            $query->where('rombongan_belajar_id', $this->rombongan_belajar_id);
+        })->orderBy('kelompok_id', 'asc')->get();
+        $pembelajaran_id = [];
+        $this->rencana_penilaian = [];
+        foreach($this->collection as $item){
+            $this->rencana_penilaian[$item->pembelajaran_id] = ($item->rapor_pts->count()) ? $item->rapor_pts->pluck('rencana_penilaian_id')->toArray() : '';
+            $pembelajaran_id[] = $item->pembelajaran_id;
+        }
+        $this->dispatchBrowserEvent('rencana_penilaian', [
+            'pembelajaran_id' => $pembelajaran_id,
+            'rencana_penilaian' => $this->collection,
+            'rencana_penilaian_select' => $this->rencana_penilaian,
+        ]);
+        $this->dispatchBrowserEvent('pharaonic.select2.init');
+        /*$this->data_siswa = Peserta_didik::whereHas('anggota_rombel', function($query){
+            $query->where('rombongan_belajar_id', $this->rombongan_belajar_id);
+        })->with(['anggota_rombel' => function($query){
+            $query->where('rombongan_belajar_id', $this->rombongan_belajar_id);
+        }])->orderBy('nama')->get();
+        $this->rombongan_belajar = Rombongan_belajar::with([
+            'kurikulum'
+        ])->find($this->rombongan_belajar_id);*/
+        $this->show = TRUE;
+    }
     public function store(){
         $pembelajaran_id_array = [];
+        $rencana_penilaian_id_array = [];
         foreach($this->rencana_penilaian as $pembelajaran_id => $rencana_penilaian){
             $pembelajaran_id_array[] = $pembelajaran_id;
             if($rencana_penilaian){
@@ -87,10 +154,18 @@ class RaporUts extends Component
             $query->whereIn('pembelajaran_id', $pembelajaran_id_array);
             $query->whereNotIn('rencana_penilaian_id', $rencana_penilaian_id_array);
         })->delete();
-        $this->alert('success', 'Rapor UTS berhasil disimpan', [
-            'showConfirmButton' => true,
-            'confirmButtonText' => 'OK',
-            'onConfirmed' => 'confirmed' 
-        ]);
+        if($rencana_penilaian_id_array){
+            $this->alert('success', 'Rapor UTS berhasil disimpan', [
+                'showConfirmButton' => true,
+                'confirmButtonText' => 'OK',
+                'onConfirmed' => 'confirmed' 
+            ]);
+        } else {
+            $this->alert('error', 'Tidak ada Rapor UTS disimpan', [
+                'showConfirmButton' => true,
+                'confirmButtonText' => 'OK',
+                'onConfirmed' => 'confirmed' 
+            ]);
+        }
     }
 }
--- resources/views/livewire/laporan/rapor-uts.blade.php
+++ resources/views/livewire/laporan/rapor-uts.blade.php
@@ -4,12 +4,16 @@
         <div class="card">
             <form wire:ignore.self wire:submit.prevent="store">
                 <div class="card-body">
+                    @role('waka', session('semester_id'))
+                        @include('livewire.formulir-waka')
+                    @endrole
+                    @if($show)
                     <input type="hidden" wire:model="rombongan_belajar_id">
                     <table class="table">
                         <thead>
                             <tr>
                                 <th class="text-center">No</th>
-                                <th>a</th>
+                                <th class="text-center">Kelas</th>
                                 <th class="text-center">Mata Pelajaran</th>
                                 <th class="text-center">Guru Mata Pelajaran</th>
                                 <th class="text-center">Pilih Penilaian</th>
@@ -17,10 +21,7 @@
                         </thead>
                         <tbody>
                             <?php $check = 0; ?>
-                            @foreach ($collection as $item)
-                            @if(count($item->rapor_pts))
-                                <?php $check++; ?>
-                            @endif
+                            @forelse ($collection as $item)
                             <tr>
                                 <td class="text-center">{{$loop->iteration}}</td>
                                 <td>{{$item->rombongan_belajar->nama}}</td>
@@ -28,7 +29,7 @@
                                 <td>{{$item->guru->nama}}</td>
                                 <td>
                                     <div wire:ignore>
-                                        <select class="form-select form-select-sm" data-pharaonic="select2" data-component-id="{{ $this->id }}" wire:model="rencana_penilaian.{{$item->pembelajaran_id}}" data-tags="true" data-placeholder="Pilih Penilaian" multiple>
+                                        <select id="rencana_penilaian_{{$check}}" class="form-select form-select-sm" data-pharaonic="select2" data-component-id="{{ $this->id }}" wire:model="rencana_penilaian.{{$item->pembelajaran_id}}" data-tags="true" data-placeholder="Pilih Penilaian" multiple>
                                             @foreach ($item->rencana_penilaian as $rencana_penilaian)
                                             <option value="{{$rencana_penilaian->rencana_penilaian_id}}">{{$rencana_penilaian->nama_penilaian}}</option>
                                             @endforeach
@@ -36,17 +37,70 @@
                                     </div>
                                 </td>
                             </tr>
+                            @if(count($item->rapor_pts))
+                                <?php $check++; ?>
+                            @endif
+                            @empty
+                            <tr>
+                                <td class="text-center" colspan="5">Tidak ada data untuk ditampilkan</td>
+                            </tr> 
+                            @endforelse
+                            @foreach ($collection as $item)
+                            
                             @endforeach
                         </tbody>
                     </table>
+                    @endif
                 </div>
+                @if($show)
                 <div class="card-footer d-md-flex justify-content-between">
                     @if($check)
                     <a target="_blank" class="btn btn-warning" href="{{url('cetak/rapor-uts/'.$rombongan_belajar_id)}}"><i class="fa fa-print"></i> Cetak</a>
                     @endif
                     <button type="submit" class="btn btn-primary">Simpan</button>
                 </div>
+                @endif
             </form>
         </div>
     </div>
 </div>
+@push('scripts')
+<script>
+    window.addEventListener('rencana_penilaian', event => {
+        //console.log(event.detail);
+        $.each(event.detail.pembelajaran_id, function (urut, pembelajaran_id) {
+            /*$('#rencana_penilaian_'+pembelajaran_id).append($('<option>', { 
+                value: item.rombongan_belajar_id,
+                text : item.nama
+            }));*/
+            $('#rencana_penilaian_'+urut).html('<option value="">== Pilih Penilaian Update ==</option>')
+            $.each(event.detail.rencana_penilaian, function (i, item) {
+                if(item.rencana_penilaian.length){
+                    $.each(item.rencana_penilaian, function (i, rencana_penilaian) {
+                        $('#rencana_penilaian_'+urut).append($('<option>', { 
+                            value: rencana_penilaian.rencana_penilaian_id,
+                            text : rencana_penilaian.nama_penilaian
+                        }));
+                    });
+                } else {
+                    $('#rencana_penilaian_'+urut).val('')
+                }
+            });
+            $.each(event.detail.rencana_penilaian_select, function (i, rencana_penilaian_select) {
+                $.each(rencana_penilaian_select, function (i, rencana_penilaian_id) {
+                    //console.log(rencana_penilaian_id);
+                    $('#rencana_penilaian_'+urut).val(rencana_penilaian_id)
+                });
+            });
+            console.log(event.detail.rencana_penilaian);
+        });
+        /*$('#rombongan_belajar_id').html('<option value="">== Pilih Penilaian ==</option>')
+        $.each(event.detail.data_rombongan_belajar, function (i, item) {
+            $('#rombongan_belajar_id').append($('<option>', { 
+                value: item.rombongan_belajar_id,
+                text : item.nama
+            }));
+        });*/
+    })
+</script>
+@endpush
