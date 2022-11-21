87288f7ddd98e6054cfa9e057fbfa83744cdd006
masadi <masadi.com@gmail.com>
2022-11-21 21:14:09 +0700

    Update Mata_pelajaran_kurikulumSeeder.php

--- database/seeders/Mata_pelajaran_kurikulumSeeder.php
+++ database/seeders/Mata_pelajaran_kurikulumSeeder.php
@@ -21,22 +21,25 @@ class Mata_pelajaran_kurikulumSeeder extends Seeder
 			$json = File::get('database/data/mata_pelajaran_kurikulum-'.$i.'.json');
 			$data = json_decode($json);
 			foreach($data as $obj){
-				DB::table('ref.mata_pelajaran_kurikulum')->insert([
-					'kurikulum_id' 	=> $obj->kurikulum_id,
-					'mata_pelajaran_id' => $obj->mata_pelajaran_id,
-					'tingkat_pendidikan_id' => $obj->tingkat_pendidikan_id,
-					'jumlah_jam' 			=> $obj->jumlah_jam,
-					'jumlah_jam_maksimum'	=> $obj->jumlah_jam_maksimum,
-					'wajib'			=> $obj->wajib,
-					'sks' => $obj->sks,
-					'a_peminatan' => $obj->a_peminatan,
-					'area_kompetensi' => $obj->area_kompetensi,
-					'gmp_id' => $obj->gmp_id,
-					'created_at'			=> $obj->create_date,
-					'updated_at'			=> $obj->last_update,
-					'deleted_at'			=> $obj->expired_date,
-					'last_sync'				=> $obj->last_sync,
-				]);
+				$find = DB::table('ref.kurikulum')->where('kurikulum_id', $obj->kurikulum_id)->first();
+				if($find){
+					DB::table('ref.mata_pelajaran_kurikulum')->insert([
+						'kurikulum_id' 	=> $obj->kurikulum_id,
+						'mata_pelajaran_id' => $obj->mata_pelajaran_id,
+						'tingkat_pendidikan_id' => $obj->tingkat_pendidikan_id,
+						'jumlah_jam' 			=> $obj->jumlah_jam,
+						'jumlah_jam_maksimum'	=> $obj->jumlah_jam_maksimum,
+						'wajib'			=> $obj->wajib,
+						'sks' => $obj->sks,
+						'a_peminatan' => $obj->a_peminatan,
+						'area_kompetensi' => $obj->area_kompetensi,
+						'gmp_id' => $obj->gmp_id,
+						'created_at'			=> $obj->create_date,
+						'updated_at'			=> $obj->last_update,
+						'deleted_at'			=> $obj->expired_date,
+						'last_sync'				=> $obj->last_sync,
+					]);
+				}
 			}
 		}
     }
