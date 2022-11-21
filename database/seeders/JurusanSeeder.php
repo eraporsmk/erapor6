fa2fd8255f6c4f48d7e63b7ddd1580112246731f
masadi <masadi.com@gmail.com>
2022-11-21 21:07:33 +0700

    Update JurusanSeeder.php

--- database/seeders/JurusanSeeder.php
+++ database/seeders/JurusanSeeder.php
@@ -19,29 +19,32 @@ class JurusanSeeder extends Seeder
         DB::table('ref.jurusan')->truncate();
 		$json = File::get('database/data/jurusan.json');
 		$data = json_decode($json);
-		Eloquent::unguard();
-		DB::statement('BEGIN;');
-		DB::statement('ALTER TABLE ref.jurusan DISABLE TRIGGER ALL;');
+		//Eloquent::unguard();
+		//DB::statement('BEGIN;');
+		//DB::statement('ALTER TABLE ref.jurusan DISABLE TRIGGER ALL;');
         foreach($data as $obj){
-			DB::table('ref.jurusan')->updateOrInsert([
-				'jurusan_id' 			=> trim($obj->jurusan_id),
-				'nama_jurusan' 			=> $obj->nama_jurusan,
-				'nama_jurusan_en' 		=> NULL,
-				'untuk_sma'				=> $obj->untuk_sma,
-				'untuk_smk'				=> $obj->untuk_smk,
-				'untuk_pt'				=> $obj->untuk_pt,
-				'untuk_slb'				=> $obj->untuk_slb,
-				'untuk_smklb'			=> $obj->untuk_smklb,
-				'jenjang_pendidikan_id'	=> $obj->jenjang_pendidikan_id,
-				'jurusan_induk'			=> $obj->jurusan_induk,
-				'level_bidang_id'		=> $obj->level_bidang_id,
-				'created_at' 			=> $obj->create_date,
-				'updated_at' 			=> $obj->last_update,
-				'deleted_at'			=> $obj->expired_date,
-				'last_sync'				=> $obj->last_sync,
-			]);
+			$find = DB::table('ref.jurusan')->find($obj->jurusan_induk);
+			if($find){
+				DB::table('ref.jurusan')->updateOrInsert([
+					'jurusan_id' 			=> trim($obj->jurusan_id),
+					'nama_jurusan' 			=> $obj->nama_jurusan,
+					'nama_jurusan_en' 		=> NULL,
+					'untuk_sma'				=> $obj->untuk_sma,
+					'untuk_smk'				=> $obj->untuk_smk,
+					'untuk_pt'				=> $obj->untuk_pt,
+					'untuk_slb'				=> $obj->untuk_slb,
+					'untuk_smklb'			=> $obj->untuk_smklb,
+					'jenjang_pendidikan_id'	=> $obj->jenjang_pendidikan_id,
+					'jurusan_induk'			=> $obj->jurusan_induk,
+					'level_bidang_id'		=> $obj->level_bidang_id,
+					'created_at' 			=> $obj->create_date,
+					'updated_at' 			=> $obj->last_update,
+					'deleted_at'			=> $obj->expired_date,
+					'last_sync'				=> $obj->last_sync,
+				]);
+			}
     	}
-		DB::statement('ALTER TABLE ref.jurusan ENABLE TRIGGER ALL;');
-		DB::statement('COMMIT;');
+		//DB::statement('ALTER TABLE ref.jurusan ENABLE TRIGGER ALL;');
+		//DB::statement('COMMIT;');
     }
 }
