dc082caf64306a249724c308612c959d37a565d2
masadi <masadi.com@gmail.com>
2022-11-22 12:54:59 +0700

    Update MataPelajaranSeeder.php

--- database/seeders/MataPelajaranSeeder.php
+++ database/seeders/MataPelajaranSeeder.php
@@ -20,21 +20,19 @@ class MataPelajaranSeeder extends Seeder
 		$data = json_decode($json);
         foreach($data as $obj){
 			$find = DB::table('ref.jurusan')->where('jurusan_id', $obj->jurusan_id)->first();
-			if($find){
-				DB::table('ref.mata_pelajaran')->insert([
-					'mata_pelajaran_id' 	=> $obj->mata_pelajaran_id,
-					'nama' 					=> $obj->nama,
-					'pilihan_sekolah'		=> $obj->pilihan_sekolah,
-					'pilihan_buku' 			=> $obj->pilihan_buku,
-					'pilihan_kepengawasan'	=> $obj->pilihan_kepengawasan,
-					'pilihan_evaluasi'		=> $obj->pilihan_evaluasi,
-					'jurusan_id'			=> $obj->jurusan_id,
-					'created_at'			=> $obj->create_date,
-					'updated_at'			=> $obj->last_update,
-					'deleted_at'			=> $obj->expired_date,
-					'last_sync'				=> $obj->last_sync,
-				]);
-			}
+			DB::table('ref.mata_pelajaran')->insert([
+				'mata_pelajaran_id' 	=> $obj->mata_pelajaran_id,
+				'nama' 					=> $obj->nama,
+				'pilihan_sekolah'		=> $obj->pilihan_sekolah,
+				'pilihan_buku' 			=> $obj->pilihan_buku,
+				'pilihan_kepengawasan'	=> $obj->pilihan_kepengawasan,
+				'pilihan_evaluasi'		=> $obj->pilihan_evaluasi,
+				'jurusan_id'			=> ($find) ? $obj->jurusan_id : NULL,
+				'created_at'			=> $obj->create_date,
+				'updated_at'			=> $obj->last_update,
+				'deleted_at'			=> $obj->expired_date,
+				'last_sync'				=> $obj->last_sync,
+			]);
     	}
     }
 }
