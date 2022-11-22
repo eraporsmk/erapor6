745a14bdc836b3e31d41629116186582a29e07bf
masadi <masadi.com@gmail.com>
2022-11-22 16:32:43 +0700

    Update seeder wilayah

--- database/seeders/Mst_wilayahSeeder.php
+++ database/seeders/Mst_wilayahSeeder.php
@@ -19,9 +19,9 @@ class Mst_wilayahSeeder extends Seeder
 		//DB::table('ref.mst_wilayah')->truncate();
 		//DB::table('ref.negara')->truncate();
         //DB::table('ref.level_wilayah')->truncate();
-		$this->command->getOutput()->progressStart(99369);
 		$json = File::get('database/data/negara.json');
 		$data = json_decode($json);
+		$this->command->getOutput()->progressStart(count($data));
         foreach($data as $obj){
     		DB::table('ref.negara')->updateOrinsert(
 				[
@@ -38,8 +38,10 @@ class Mst_wilayahSeeder extends Seeder
 			);
 			$this->command->getOutput()->progressAdvance();
     	}
+		$this->command->getOutput()->progressFinish();
 		$json = File::get('database/data/level_wilayah.json');
 		$data = json_decode($json);
+		$this->command->getOutput()->progressStart(count($data));
         foreach($data as $obj){
     		DB::table('ref.level_wilayah')->updateOrinsert(
 				[
@@ -55,10 +57,12 @@ class Mst_wilayahSeeder extends Seeder
 			);
 			$this->command->getOutput()->progressAdvance();
     	}
-		for($i=0;$i<=4;$i++){
+		$this->command->getOutput()->progressFinish();
+		for($i=0;$i<=3;$i++){
 			$json = File::get('database/data/mst_wilayah_'.$i.'.json');
 			$data = json_decode($json);
-			$this->command->info('database/data/mst_wilayah_'.$i.'.json => '.count($data));
+			$this->command->getOutput()->progressStart(count($data));
+			$this->command->info("\n".'database/data/mst_wilayah_'.$i.'.json => '.count($data));
 			foreach($data as $obj){
 				DB::table('ref.mst_wilayah')->updateOrinsert(
 					[
@@ -81,7 +85,36 @@ class Mst_wilayahSeeder extends Seeder
 				);
 				$this->command->getOutput()->progressAdvance();
 			}
+			$this->command->getOutput()->progressFinish();
 		}
-		$this->command->getOutput()->progressFinish();
+		for($i=1;$i<=183;$i++){
+			$json = File::get('database/data/mst_wilayah_4-'.$i.'.json');
+			$data = json_decode($json);
+			$this->command->getOutput()->progressStart(count($data));
+			$this->command->info("\n".'database/data/mst_wilayah_4-'.$i.'.json => '.count($data));
+			foreach($data as $obj){
+				DB::table('ref.mst_wilayah')->updateOrinsert(
+					[
+						'kode_wilayah' 	=> $obj->kode_wilayah,
+					],
+					[
+						'nama' => $obj->nama,
+						'id_level_wilayah' => $obj->id_level_wilayah,
+						'mst_kode_wilayah' 			=> $obj->mst_kode_wilayah,
+						'negara_id'	=> $obj->negara_id,
+						'asal_wilayah'			=> $obj->asal_wilayah,
+						'kode_bps' => $obj->kode_bps,
+						'kode_dagri' => $obj->kode_dagri,
+						'kode_keu' => $obj->kode_keu,
+						'created_at'			=> $obj->create_date,
+						'updated_at'			=> $obj->last_update,
+						'deleted_at'			=> $obj->expired_date,
+						'last_sync'				=> $obj->last_sync,
+					]
+				);
+				$this->command->getOutput()->progressAdvance();
+			}
+			$this->command->getOutput()->progressFinish();
+		}
     }
 }
