f5dc52f6a1e03cd103a259077174845a8edcd2a9
masadi <masadi.com@gmail.com>
2022-11-22 15:48:51 +0700

    Update wilaya seeder

--- database/seeders/Mst_wilayahSeeder.php
+++ database/seeders/Mst_wilayahSeeder.php
@@ -22,48 +22,60 @@ class Mst_wilayahSeeder extends Seeder
 		$json = File::get('database/data/negara.json');
 		$data = json_decode($json);
         foreach($data as $obj){
-    		DB::table('ref.negara')->updateOrinsert([
-    			'negara_id' 			=> $obj->negara_id,
-    			'nama' 		=> $obj->nama,
-				'luar_negeri'			=> $obj->luar_negeri,
-				'created_at' 			=> $obj->create_date,
-				'updated_at' 			=> $obj->last_update,
-				'deleted_at'			=> $obj->expired_date,
-				'last_sync'				=> $obj->last_sync,
-    		]);
+    		DB::table('ref.negara')->updateOrinsert(
+				[
+					'negara_id' 			=> $obj->negara_id,
+				],
+				[
+					'nama' 		=> $obj->nama,
+					'luar_negeri'			=> $obj->luar_negeri,
+					'created_at' 			=> $obj->create_date,
+					'updated_at' 			=> $obj->last_update,
+					'deleted_at'			=> $obj->expired_date,
+					'last_sync'				=> $obj->last_sync,
+				]
+			);
     	}
 		$json = File::get('database/data/level_wilayah.json');
 		$data = json_decode($json);
         foreach($data as $obj){
-    		DB::table('ref.level_wilayah')->updateOrinsert([
-    			'id_level_wilayah' 			=> $obj->id_level_wilayah,
-    			'level_wilayah' 		=> $obj->level_wilayah,
-    			'created_at' 			=> $obj->create_date,
-				'updated_at' 			=> $obj->last_update,
-				'deleted_at'			=> $obj->expired_date,
-				'last_sync'				=> $obj->last_sync,
-    		]);
+    		DB::table('ref.level_wilayah')->updateOrinsert(
+				[
+					'id_level_wilayah' 			=> $obj->id_level_wilayah,
+				],
+				[
+					'level_wilayah' 		=> $obj->level_wilayah,
+					'created_at' 			=> $obj->create_date,
+					'updated_at' 			=> $obj->last_update,
+					'deleted_at'			=> $obj->expired_date,
+					'last_sync'				=> $obj->last_sync,
+				]
+			);
     	}
 		for($i=0;$i<=4;$i++){
 			//$this->command->info($i);
 			$json = File::get('database/data/mst_wilayah_'.$i.'.json');
 			$data = json_decode($json);
 			foreach($data as $obj){
-				DB::table('ref.mst_wilayah')->updateOrinsert([
-					'kode_wilayah' 	=> $obj->kode_wilayah,
-					'nama' => $obj->nama,
-					'id_level_wilayah' => $obj->id_level_wilayah,
-					'mst_kode_wilayah' 			=> $obj->mst_kode_wilayah,
-					'negara_id'	=> $obj->negara_id,
-					'asal_wilayah'			=> $obj->asal_wilayah,
-					'kode_bps' => $obj->kode_bps,
-					'kode_dagri' => $obj->kode_dagri,
-					'kode_keu' => $obj->kode_keu,
-					'created_at'			=> $obj->create_date,
-					'updated_at'			=> $obj->last_update,
-					'deleted_at'			=> $obj->expired_date,
-					'last_sync'				=> $obj->last_sync,
-				]);
+				DB::table('ref.mst_wilayah')->updateOrinsert(
+					[
+						'kode_wilayah' 	=> $obj->kode_wilayah,
+					],
+					[
+						'nama' => $obj->nama,
+						'id_level_wilayah' => $obj->id_level_wilayah,
+						'mst_kode_wilayah' 			=> $obj->mst_kode_wilayah,
+						'negara_id'	=> $obj->negara_id,
+						'asal_wilayah'			=> $obj->asal_wilayah,
+						'kode_bps' => $obj->kode_bps,
+						'kode_dagri' => $obj->kode_dagri,
+						'kode_keu' => $obj->kode_keu,
+						'created_at'			=> $obj->create_date,
+						'updated_at'			=> $obj->last_update,
+						'deleted_at'			=> $obj->expired_date,
+						'last_sync'				=> $obj->last_sync,
+					]
+				);
 			}
 		}
     }
