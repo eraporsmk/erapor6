145021b436c740b9dba5e602db1b8270434d7e4b
masadi <masadi.com@gmail.com>
2022-11-26 07:47:50 +0700

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL').'/storage'.env('APP_PUBLIC_STORAGE'),
             'visibility' => 'public',
         ],
 
