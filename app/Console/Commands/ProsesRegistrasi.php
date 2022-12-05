1d4fb5554748ebbab88148d6399c5bc561d27930
eRaporSMK <53444184+eraporsmk@users.noreply.github.com>
2022-12-05 14:16:20 +0700

    Register via CMD

---
+++ app/Console/Commands/ProsesRegistrasi.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Console\Command;
+use App\Models\User;
+use App\Models\Role;
+use App\Models\Semester;
+use App\Models\Mst_wilayah;
+use App\Models\Sekolah;
+use Validator;
+
+class ProsesRegistrasi extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'erapor:register';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $npsn = $this->inputan('NPSN');
+        //$this->ask('NPSN');
+        $email = $this->inputan('Email Administrator');
+        $password = $this->inputan('Password Dapodik');
+        try {
+            $data_sync = [
+                'npsn' => $npsn,
+                'email' => $email,
+                'password' => $password,
+            ];
+            $response = Http::withBasicAuth('admin', '1234')->asForm()->post('http://app.erapor-smk.net/api/register', $data_sync);
+            $data = $response->object();
+            if($response->successful()){
+                return $this->create_user($data, $email, $password);
+            } else {
+                $this->error($data->message);
+            }
+        } catch (\Exception $e){
+            $this->error($e->getMessage());
+        }
+		$this->info($npsn);
+        $this->info($email);
+        $this->info($password);
+    }
+    private function inputan($input){
+        $data = $this->ask($input);
+        if(!$data){
+            $this->error($input.' tidak boleh kosong!');
+            return $this->inputan($input);
+        }
+        return $data;
+    }
+    private function create_user($data, $email, $password){
+        if(!$data->data){
+            $this->error($data->message);
+        }
+        $set_data = $data->data;
+        if($set_data->bentuk_pendidikan_id == '15'){
+            $get_kode_wilayah = $set_data->wilayah;
+            $kode_wilayah = $set_data->kode_wilayah;
+            $kecamatan = '-';
+            $kabupaten = '-';
+            $provinsi = '-';
+            if($get_kode_wilayah){
+                $kode_wilayah = $get_kode_wilayah->kode_wilayah;
+                if($get_kode_wilayah->parrent_recursive){
+                    $kecamatan = $get_kode_wilayah->parrent_recursive->nama;
+                    if($get_kode_wilayah->parrent_recursive->parrent_recursive){
+                        $kabupaten = $get_kode_wilayah->parrent_recursive->parrent_recursive->nama;
+                        if($get_kode_wilayah->parrent_recursive->parrent_recursive->parrent_recursive){
+                            $provinsi = $get_kode_wilayah->parrent_recursive->parrent_recursive->parrent_recursive->nama;
+                            Mst_wilayah::updateOrCreate(
+                                [
+                                    'kode_wilayah' => $get_kode_wilayah->parrent_recursive->parrent_recursive->parrent_recursive->kode_wilayah,
+                                ],
+                                [
+                                    'nama' => $get_kode_wilayah->parrent_recursive->parrent_recursive->parrent_recursive->nama,
+                                    'id_level_wilayah' => $get_kode_wilayah->parrent_recursive->parrent_recursive->parrent_recursive->id_level_wilayah,
+                                    'mst_kode_wilayah' => $get_kode_wilayah->parrent_recursive->parrent_recursive->parrent_recursive->mst_kode_wilayah,
+                                    'negara_id' => $get_kode_wilayah->parrent_recursive->parrent_recursive->parrent_recursive->negara_id,
+                                    'last_sync' => now(),
+                                ]
+                            );
+                        }
+                        Mst_wilayah::updateOrCreate(
+                            [
+                                'kode_wilayah' => $get_kode_wilayah->parrent_recursive->parrent_recursive->kode_wilayah,
+                            ],
+                            [
+                                'nama' => $get_kode_wilayah->parrent_recursive->parrent_recursive->nama,
+                                'id_level_wilayah' => $get_kode_wilayah->parrent_recursive->parrent_recursive->id_level_wilayah,
+                                'mst_kode_wilayah' => $get_kode_wilayah->parrent_recursive->parrent_recursive->mst_kode_wilayah,
+                                'negara_id' => $get_kode_wilayah->parrent_recursive->parrent_recursive->negara_id,
+                                'last_sync' => now(),
+                            ]
+                        );
+                    }
+                    Mst_wilayah::updateOrCreate(
+                        [
+                            'kode_wilayah' => $get_kode_wilayah->parrent_recursive->kode_wilayah,
+                        ],
+                        [
+                            'nama' => $get_kode_wilayah->parrent_recursive->nama,
+                            'id_level_wilayah' => $get_kode_wilayah->parrent_recursive->id_level_wilayah,
+                            'mst_kode_wilayah' => $get_kode_wilayah->parrent_recursive->mst_kode_wilayah,
+                            'negara_id' => $get_kode_wilayah->parrent_recursive->negara_id,
+                            'last_sync' => now(),
+                        ]
+                    );
+                }
+                Mst_wilayah::updateOrCreate(
+                    [
+                        'kode_wilayah' => $get_kode_wilayah->kode_wilayah,
+                    ],
+                    [
+                        'nama' => $get_kode_wilayah->nama,
+                        'id_level_wilayah' => $get_kode_wilayah->id_level_wilayah,
+                        'mst_kode_wilayah' => $get_kode_wilayah->mst_kode_wilayah,
+                        'negara_id' => $get_kode_wilayah->negara_id,
+                        'last_sync' => now(),
+                    ]
+                );
+            }
+            $sekolah = Sekolah::updateOrCreate(
+                ['sekolah_id' => $set_data->sekolah_id],
+                [
+                    'npsn' 					=> $set_data->npsn,
+                    'nss' 					=> $set_data->nss,
+                    'nama' 					=> $set_data->nama,
+                    'alamat' 				=> $set_data->alamat_jalan,
+                    'desa_kelurahan'		=> $set_data->desa_kelurahan,
+                    'kode_wilayah'			=> $kode_wilayah,
+                    'kecamatan' 			=> $kecamatan,
+                    'kabupaten' 			=> $kabupaten,
+                    'provinsi' 				=> $provinsi,
+                    'kode_pos' 				=> $set_data->kode_pos,
+                    'lintang' 				=> $set_data->lintang,
+                    'bujur' 				=> $set_data->bujur,
+                    'no_telp' 				=> $set_data->nomor_telepon,
+                    'no_fax' 				=> $set_data->nomor_fax,
+                    'email' 				=> $set_data->email,
+                    'website' 				=> $set_data->website,
+                    'status_sekolah'		=> $set_data->status_sekolah,
+                    'last_sync'				=> now(),
+                ]
+            );
+            $semester = Semester::where('periode_aktif', 1)->first();
+            $user = User::create([
+                'sekolah_id' => $sekolah->sekolah_id,
+                'name' => 'Administrator',
+                'email' => $email,
+                'password' => bcrypt($password),
+            ]);
+            $adminRole = Role::where('name', 'admin')->first();
+            $team = Team::updateOrCreate([
+                'name' => $semester->nama,
+                'display_name' => $semester->nama,
+                'description' => $semester->nama,
+            ]);
+            $user->attachRole($adminRole, $team);
+            $this->error('Registrasi berhasil');
+        } else {
+            $this->error('Jenjang Sekolah Salah');
+        }
+    }
+}
